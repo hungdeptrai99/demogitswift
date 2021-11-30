b33202651f0d405e2ff48451c4a1e98408f5fabf
Hungtg <hungtg@riikeisoft.com>
2021-11-30 09:57:09 +0700

    Initial Commit

---
+++ LearnSwift/LearnSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnSwiftApp.swift
+//  LearnSwift
+//
+//  Created by HungTG on 30/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct LearnSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
